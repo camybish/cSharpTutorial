7ec300913eb7579480af5693052d10457a15fe39
Cameron Bishop <cameronjamesbishop@googlemail.com>
2023-09-22 11:47:00 +0100

    The wildest fizzbuzz, also accidentally skipped chapter 3

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,20 +1,56 @@
-﻿Console.WriteLine("Signed integral types:");
+﻿// SKU = Stock Keeping Unit. 
+// SKU value format: <product #>-<2-letter color code>-<size code>
+string sku = "01-MN-L";
 
-Console.WriteLine($"sbyte  : {sbyte.MinValue} to {sbyte.MaxValue}");
-Console.WriteLine($"short  : {short.MinValue} to {short.MaxValue}");
-Console.WriteLine($"int    : {int.MinValue} to {int.MaxValue}");
-Console.WriteLine($"long   : {long.MinValue} to {long.MaxValue}");
+string[] product = sku.Split('-');
 
-Console.WriteLine("");
-Console.WriteLine("Unsigned integral types:");
+string type = "";
+string color = "";
+string size = "";
 
-Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
-Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
-Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
-Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
+switch(product[0])
+{
+    case "01":
+        type = "Sweat shirt";
+        break;
+    case "02":
+        type = "T-Shirt";
+        break;
+    case "03":
+        type = "Sweat pants";
+        break;
+    default:
+        type = "Other";
+        break;
+}
 
-Console.WriteLine("");
-Console.WriteLine("Floating point types:");
-Console.WriteLine($"float  : {float.MinValue} to {float.MaxValue} (with ~6-9 digits of precision)");
-Console.WriteLine($"double : {double.MinValue} to {double.MaxValue} (with ~15-17 digits of precision)");
-Console.WriteLine($"decimal: {decimal.MinValue} to {decimal.MaxValue} (with 28-29 digits of precision)");
+switch(product[1])
+{
+    case "BL":
+        color = "Black";
+        break;
+    case "MN":
+        color = "Maroon";
+        break;
+    default:
+        color = "White";
+        break;
+}
+
+switch(product[2])
+{
+    case "S":
+        size = "Small";
+        break;
+    case "M":
+        size = "Medium";
+        break;
+    case "L":
+        size = "Large";
+        break;
+    default:
+        size = "One Size Fits All";
+        break;
+}
+
+Console.WriteLine($"Product: {size} {color} {type}");
---
+++ fizzBuzz/Program.cs
@@ -0,0 +1,23 @@
+﻿for(int i = 1; i <= 100; i++)
+{
+    switch(i % 15)
+    {
+        case 3:
+        case 6:
+        case 9:
+        case 12:
+            Console.WriteLine($"{i} - Fizz");
+            break;
+        case 5:
+        case 10:
+            Console.WriteLine($"{i} - Buzz");
+            break;
+        case 0:
+            Console.WriteLine($"{i} - FizzBuzz");
+            break;
+        default:        
+            Console.WriteLine(i);
+            break;
+    }
+    
+}
